ae936e429bc62be8d9211bf168f43424d4dc2430
jciado-eng <jhonnyciado@gmail.com>
2026-09-06 15:09:56 -0400

    altro

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import './App.css';
 
 import Navbar from './Components/Navbar/Navbar';
 import Landing_Page from './Components/Landing_Page/Landing_Page';
+import Login from './Components/Login/Login';
+import Sign_Up from './Components/Sign_Up/Sign_Up';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Landing_Page />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Sign_Up />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const API_URL = window.location.hostname === "localhost" ? "https://henry99-8181.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/" : "https://henry99-8181.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/";
+export const API_URL = window.location.hostname === "localhost" ? "https://henry99-4174.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/" : "https://henry99-4174.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/";
 console.log(
     "API_URL :",
     API_URL
