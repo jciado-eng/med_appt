3e18cc7220508366b923fe6b281a3159426b8820
jciado-eng <jhonnyciado@gmail.com>
2026-09-07 06:00:47 -0400

    mod

--- src/Components/DoctorCard/DoctorCard.jsx
+++ src/Components/DoctorCard/DoctorCard.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
-import './DoctorCardIC.css';
+import './DoctorCard.css';
 import AppointmentFormIC from '../AppointmentFormIC/AppointmentFormIC';
 import { v4 as uuidv4 } from 'uuid';
 
