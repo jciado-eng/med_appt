9e41a11072bbd7d6f7694880f65d279828aad341
jciado-eng <jhonnyciado@gmail.com>
2026-09-07 05:08:11 -0400

    altro

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Landing_Page from './Components/Landing_Page/Landing_Page';
 import Login from './Components/Login/Login';
 import Sign_Up from './Components/Sign_Up/Sign_Up';
 
-import InstantConsultation from './Components/InstantConsultation/InstantConsultation'; 
+import InstantConsultation from './components/InstantConsultation/InstantConsultation';  
 
 function App() {
   return (
@@ -18,6 +18,7 @@ function App() {
           <Route path="/" element={<Landing_Page />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Sign_Up />} />
+          
           <Route path="/instant-consultation" element={<InstantConsultation />} />
         </Routes>
       </BrowserRouter>
