9d695813c53522c34c67e7b81d7196c19fa80279
jciado-eng <jhonnyciado@gmail.com>
2026-09-09 10:02:27 -0400

    a

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Landing_Page from './Components/Landing_Page/Landing_Page';
 import Login from './Components/Login/Login';
 import Sign_Up from './Components/Sign_Up/Sign_Up';
 
-import InstantConsultation from './Components/InstantConsultation/InstantConsultation';  
+import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultation';  
 import Notification from './Components/Notification/Notification';
 
 function App() {
