b4ae24e2e26bbbe6291d1264d048c78d411adcea
jciado-eng <jhonnyciado@gmail.com>
2026-09-09 10:37:56 -0400

    altro

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,23 @@ import Navbar from './Components/Navbar/Navbar';
 import Landing_Page from './Components/Landing_Page/Landing_Page';
 import Login from './Components/Login/Login';
 import Sign_Up from './Components/Sign_Up/Sign_Up';
-
-import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultation';  
+import InstantConsultation from './Components/InstantConsultation/InstantConsultation';  
 import Notification from './Components/Notification/Notification';
+import ReviewForm from './Components/ReviewForm/ReviewForm';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Notification>
+          <Navbar /> 
           <Routes>
             <Route path="/" element={<Landing_Page />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Sign_Up />} />
             <Route path="/instant-consultation" element={<InstantConsultation />} />
             <Route path="/notifications" element={<Notification />} />
+            <Route path="/reviews" element={<ReviewForm />} />
           </Routes>
         </Notification>
       </BrowserRouter>
